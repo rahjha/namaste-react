e5bf8b3fed7e342f4223eba23fd40f0e261e9054
rahjha <rahjha25@gmail.com>
2023-03-24 00:11:21 +0530

    episode - 3 | lying the foundation

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+const heading = (
+  <h1 id="title" key="h1">
+    Namaste React
+  </h1>
+);
 
-console.log(parent);
+const Title = () => {
+  return <h1>This is title</h1>;
+};
+const xyz = 10;
+const HeaderComponent = () => {
+  return (
+    <div>
+      {xyz}
+      <Title />
+      {heading}
+      <h1>Namaste React Functional Component</h1>
+      <h2>This is h2 tag</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeaderComponent />);
