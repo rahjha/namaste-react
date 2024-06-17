4eb97bdb02c7e3009fa4894231ff7a5f2cc8f5cd
rahjha <rahuljha.kumar25@gmail.com>
2024-06-17 11:06:43 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
-/* 
-<div id="parent">
-    <div id="child">
-        <h1>I am an h1 tag</h1>
-    </div>
-</div> 
-*/
+import React from "react";
+import ReactDOM from "react-dom";
+
 
 const parent = React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"}, [React.createElement("h1",{},"I am an h1 tag"),React.createElement("h2",{},"I am an h2 tag")]));
 
